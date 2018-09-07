3a45cf40a583b23d28a1b6d3e54e5f4b9eb0d64d
Louis Roché <louis.roche@ahrefs.com>
2018-09-07 01:04:32 +0000

    simplify roundtrip tests

--- __tests__/roundtrip_test.ml
+++ __tests__/roundtrip_test.ml
@@ -2,78 +2,62 @@
 
 open Jest
 
-type 'a test =
-  { name : string
-  ; encode : 'a -> Js.Json.t
-  ; decode : Js.Json.t -> 'a
-  ; data : 'a
-  }
-
-type test' = T : 'a test -> test'
-
-let test_roundtrip ~name ~write ~read ~data =
-  T { name
-    ; encode = Atdgen_codec_runtime.Encode.encode write
-    ; decode = Atdgen_codec_runtime.Decode.decode read
-    ; data }
-
-let run_test (T t) =
-  let json = t.encode t.data in
-  let data' = t.decode json in
-  test t.name (fun () ->
-    Expect.expect t.data
-    |> Expect.toEqual data'
+let run_test ~name ~write ~read ~data =
+  let open Expect in
+  let encode = Atdgen_codec_runtime.Encode.encode write in
+  let decode = Atdgen_codec_runtime.Decode.decode read in
+  let json = encode data in
+  let data' = decode json in
+  test name (fun () ->
+    expect data
+    |> toEqual data'
   )
 
-let run_tests tests = List.iter run_test tests
-
-let _ =
-  describe "tests" (fun () ->
-      run_tests
-        [ test_roundtrip
-            ~name:"record"
-            ~write:Test_bs.write_r
-            ~read:Test_bs.read_r
-            ~data:{Test_t.a = 1; b = "string";}
-        ; test_roundtrip
-            ~name:"record optional absent"
-            ~write:Test_bs.write_ro
-            ~read:Test_bs.read_ro
-            ~data:{Test_t.c = "s"; o = None;}
-        ; test_roundtrip
-            ~name:"record optional present"
-            ~write:Test_bs.write_ro
-            ~read:Test_bs.read_ro
-            ~data:{Test_t.c = "s"; o = Some 3L;}
-        ; test_roundtrip
-            ~name:"variant list"
-            ~write:Test_bs.write_vl
-            ~read:Test_bs.read_vl
-            ~data:[Test_t.A 1; B "s"]
-        ; test_roundtrip
-            ~name:"variant poly list"
-            ~write:Test_bs.write_vpl
-            ~read:Test_bs.read_vpl
-            ~data:[`A 1; `B "s"]
-        ; test_roundtrip
-            ~name:"tupple"
-            ~write:Test_bs.write_t
-            ~read:Test_bs.read_t
-            ~data:(1, "s", 1.1)
-        ; test_roundtrip
-            ~name:"int nullable absent"
-            ~write:Test_bs.write_n
-            ~read:Test_bs.read_n
-            ~data:None
-        ; test_roundtrip
-            ~name:"int nullable present"
-            ~write:Test_bs.write_n
-            ~read:Test_bs.read_n
-            ~data:(Some 1)
-        ; test_roundtrip
-            ~name:"int64"
-            ~write:Test_bs.write_int64
-            ~read:Test_bs.read_int64
-            ~data:3L
-        ]
+let () =
+  describe "roundtrip tests" (fun () ->
+    run_test
+      ~name:"record"
+      ~write:Test_bs.write_r
+      ~read:Test_bs.read_r
+      ~data:{Test_t.a = 1; b = "string";};
+    run_test
+      ~name:"record optional absent"
+      ~write:Test_bs.write_ro
+      ~read:Test_bs.read_ro
+      ~data:{Test_t.c = "s"; o = None;};
+    run_test
+      ~name:"record optional present"
+      ~write:Test_bs.write_ro
+      ~read:Test_bs.read_ro
+      ~data:{Test_t.c = "s"; o = Some 3L;};
+    run_test
+      ~name:"variant list"
+      ~write:Test_bs.write_vl
+      ~read:Test_bs.read_vl
+      ~data:[Test_t.A 1; B "s"];
+    run_test
+      ~name:"variant poly list"
+      ~write:Test_bs.write_vpl
+      ~read:Test_bs.read_vpl
+      ~data:[`A 1; `B "s"];
+    run_test
+      ~name:"tupple"
+      ~write:Test_bs.write_t
+      ~read:Test_bs.read_t
+      ~data:(1, "s", 1.1);
+    run_test
+      ~name:"int nullable absent"
+      ~write:Test_bs.write_n
+      ~read:Test_bs.read_n
+      ~data:None;
+    run_test
+      ~name:"int nullable present"
+      ~write:Test_bs.write_n
+      ~read:Test_bs.read_n
+      ~data:(Some 1);
+    run_test
+      ~name:"int64"
+      ~write:Test_bs.write_int64
+      ~read:Test_bs.read_int64
+      ~data:3L
   )
