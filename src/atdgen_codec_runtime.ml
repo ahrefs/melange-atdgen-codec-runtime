25f2e31f9be781de43ee2d47a0b32fa5e28bfbb7
Rusty Key <iam@stfoo.ru>
2019-01-30 15:37:29 +0300

    fix suggestion

--- src/atdgen_codec_runtime.ml
+++ src/atdgen_codec_runtime.ml
@@ -82,10 +82,7 @@ module Encode = struct
   type field = F : 'a field_spec -> field
 
   let list encode l =
-    l |> Array.of_list |> array
-    l |> Array.of_list
-      |> Array.map encode
-      |> jsonArray
+    l |> Array.of_list |> jsonArray
 
   let field ?default encode ~name data =
     F (Required (
