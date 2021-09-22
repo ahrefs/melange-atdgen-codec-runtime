71841d88a7f27e4e64ce3d85df072d671db4758a
Andrew Herron <thespyder@programmer.net>
2021-09-22 20:00:22 +1000

    Separate Json_adapter as well. Add interface files.

---
+++ src/atdgen_json_adapter.mli
@@ -0,0 +1,27 @@
+
+module type S = sig
+  (** Convert a real json tree into an atd-compliant form. *)
+  val normalize : Js.Json.t -> Js.Json.t
+
+  (** Convert an atd-compliant json tree into a real json tree. *)
+  val restore : Js.Json.t -> Js.Json.t
+end
+
+module Type_field : sig
+  module type Param = sig
+    val type_field_name : string
+  end
+
+  (** Default parameters, using [type_field_name = "type"]. *)
+  module Default_param : Param
+
+  (** Default adapter assuming a ["type"] field. *)
+  include S
+
+  (** Functor, allowing the use of a custom parameter:
+    {[
+    module Kind_field = Type_field.Make (struct let type_field_name = "kind" end)
+    ]}
+  *)
+  module Make (Param : Param) : S
+end
