466096271a4cd9e040bf822d21ca4797cafb480c
Andrew Herron <thespyder@programmer.net>
2021-09-30 08:11:38 +1000

    Apply code formatting

--- src/atdgen_json_adapter.ml
+++ src/atdgen_json_adapter.ml
@@ -1,43 +1,41 @@
 module type S = sig
-val normalize : Js.Json.t -> Js.Json.t
-val restore : Js.Json.t -> Js.Json.t
+  val normalize : Js.Json.t -> Js.Json.t
+
+  val restore : Js.Json.t -> Js.Json.t
 end
 
 module Type_field = struct
+  module type Param = sig
+    val type_field_name : string
+  end
 
-module type Param = sig
-  val type_field_name : string
-end
+  module Make (Param : Param) : S = struct
+    open Param
 
-module Make(Param:Param) : S = struct
-  open Param
-
-  let normalize (json : Js.Json.t) =
-    match json |> Js.Json.classify with
-      | JSONObject obj ->
-        begin match Js.Dict.get obj type_field_name with
+    let normalize (json : Js.Json.t) =
+      match json |> Js.Json.classify with
+      | JSONObject obj -> (
+          match Js.Dict.get obj type_field_name with
           | Some type_ ->
-            let normalized: Js.Json.t = Obj.magic (type_, json) in
-            normalized
-          | None -> json
-        end
+              let normalized : Js.Json.t = Obj.magic (type_, json) in
+              normalized
+          | None -> json)
       | _ -> json
 
-  let restore json =
-    match json |> Js.Json.classify with
-    | JSONArray [|v; o|] when Js.typeof v = "string" ->
-        begin match o |> Js.Json.classify with
+    let restore json =
+      match json |> Js.Json.classify with
+      | JSONArray [| v; o |] when Js.typeof v = "string" -> (
+          match o |> Js.Json.classify with
           | JSONObject obj ->
               Js.Dict.set obj type_field_name v;
               Json_encode.jsonDict obj
-          | _ -> json
-        end
-    | _ -> json
-end
+          | _ -> json)
+      | _ -> json
+  end
 
-module Default_param : Param = struct
-  let type_field_name = "type"
-end
+  module Default_param : Param = struct
+    let type_field_name = "type"
+  end
 
-include Make(Default_param)
+  include Make (Default_param)
 end
